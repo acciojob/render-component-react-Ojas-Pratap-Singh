80836752f130dbc8a44c2486eb42a00fac88d5fb
Ojas <singhojas3299@gmail.com>
2024-02-01 23:01:44 +0530

    done

---
+++ src/components/Learning.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Learning = () => {
+
+
+
+    return (
+        <div>
+            <p>I am learning React. My life is getting better.</p>
+        </div>
+    )
+}
+
+export default Learning
